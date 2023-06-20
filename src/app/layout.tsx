a6936b1f835a406360d5b4500dc933bf8f405adb
mcwachira <23430963+mcwachira@users.noreply.github.com>
2023-06-20 06:30:56 +0300

    initialized the app, created tyhe Navbar , Hero and Fotter components

---
+++ src/app/layout.tsx
@@ -0,0 +1,28 @@
+import './globals.css'
+import { Inter } from 'next/font/google'
+import React from "react";
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata = {
+  title: 'Myeri Car Hub',
+  description: 'Buy the best cars at the best prices',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+      <Navbar/>
+      {children}
+      <Footer/>
+      </body>
+    </html>
+  )
+}
